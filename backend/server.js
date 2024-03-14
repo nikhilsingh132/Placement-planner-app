617feacd2a0188981fba2cf787fa5934cdf439cf
nikhilsingh132 <nikhil.kumarsingh.cd.ece20@itbhu.ac.in>
2024-03-14 10:55:42 +0530

    cors changes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,13 @@ app.use(express.json());
 const cors = require("cors")
 
 //enable cors
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://placement-planner.netlify.app/"],
+        methods:["GET","POST"],
+        credentials:true,
+    }
+));
 
 app.use(require("./api/QuesApi"));
 app.use(require("./api/UserApi"))
